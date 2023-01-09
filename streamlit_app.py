68fe0fec5da9afa2577913f4c580b26d1bee4aae
TomJohnH <thasiow@onet.pl>
2023-01-09 19:40:53 +0100

    debug simple example

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,28 +17,35 @@ local_css("debug.css")
 # --------------- EXAMPLE -----------------
 
 
+st.title("This is an app")
+st.write("Check how you can debug you code and view variables directely in Streamlit:")
+
+st.write("1. Create a matrix np.matrix('1 2; 3 4')")
+a = np.matrix("1 2; 3 4")
+st.write(a)
+d.debug("this presents state of the matrix " + str(a))
+
+st.write("2. Create a counter in session state")
+
 if "counter" not in st.session_state:
     st.session_state["counter"] = 1
 
-a = np.matrix("1 2; 3 4")
+st.write(st.session_state["counter"])
+
+d.debug("counter " + str(st.session_state["counter"]))
+
 st.session_state["counter"] += 1
 
-st.title("This is an app")
-st.write("A eally cool app")
-
-k = random.randint(5, 10)
-st.write(k)
-
-d.debug(k)
-
-d.debug("counter" + str(st.session_state["counter"]))
-
-d.debug("this presents state of the matrix " + str(a))
+st.write("At the bottom you should see debug div")
+st.write(
+    "Debug div has info regarding: time of calculation, line where debug command is put and saved debug calc "
+)
 
 
 # ----- [DEBUG DIV] initize debug elements -----
 
-st.markdown(
-    f'<div class="debug">{ st.session_state["debug_string"]}</div>',
-    unsafe_allow_html=True,
-)
+if "debug_string" in st.session_state:
+    st.markdown(
+        f'<div class="debug">{ st.session_state["debug_string"]}</div>',
+        unsafe_allow_html=True,
+    )
